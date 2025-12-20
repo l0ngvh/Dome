13b2560101f9b83212b95bd4da6f323c496ef067
Vo Hoang Long <github.barricade@alias.longvh.com>
2025-12-20 17:01:52 +0900

    feat: print information on panic

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 use anyhow::Result;
 use new_wm::WindowManager;
 use tracing_error::ErrorLayer;
+use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{fmt, layer::SubscriberExt};
 
 fn main() {
     tracing_subscriber::registry()
-        .with(fmt::layer())
+        .with(fmt::layer().with_span_events(FmtSpan::ENTER))
         .with(ErrorLayer::default())
         .init();
-    println!("Window Manager starting...");
+    std::panic::set_hook(Box::new(|panic_info| {
+        let backtrace = backtrace::Backtrace::new();
+        tracing::error!("Application panicked: {panic_info}. Backtrace: {backtrace:?}");
+    }));
     if let Err(e) = run() {
         eprintln!("{e:#}");
     }
 }
 
-#[tracing::instrument]
 fn run() -> Result<()> {
     let wm = WindowManager::new()?;
     wm.list_windows()
